9f57891883a6b2054fd4c6717f0e5de4166f2f42
Jaspreet <jaspreetmehra185@gmail.com>
2024-08-31 17:31:11 +0530

    functionallity added create a docs under the docs

--- app/(main)/_components/document-list.tsx
+++ app/(main)/_components/document-list.tsx
@@ -59,7 +59,7 @@ export const DocumentList = ({
         <p 
             style={{ paddingLeft: level? `${(level * 12) + 25}px` : undefined}}
             className={cn(
-                "text-sm font-medium text-muted-foreground/80",
+                "hidden text-sm font-medium text-muted-foreground/80",
                 expanded && "last:block",
                 level === 0 && "hidden"
             )}
