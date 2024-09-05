b102abc3996485095e7bdd3387e359d0a932a5b9
Jaspreet <jaspreetmehra185@gmail.com>
2024-09-05 19:01:35 +0530

    add toolbar

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,9 +1,41 @@
-const DocumentIdPage = () => {
-    return ( 
-        <div>
-            DocumentId
-        </div>
-     );
+"use client"
+
+import { Toolbar } from "@/components/toolbar";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { useQuery } from "convex/react";
+
+interface DocumentIdProps {
+    params: {
+        documentId: Id<"documents">
+    };
 }
- 
+
+const DocumentIdPage = ({
+    params
+}: DocumentIdProps) => {
+    const document = useQuery(api.documents.getById, {
+        documentId: params.documentId
+    });
+
+    if(document == undefined){
+        return(
+            <div>Loading...</div>
+        )
+    }
+
+    if (document === null) {
+        return <div>Not found</div>
+    }
+    
+    return (
+        <div className="pb-40">
+            <div className="h-[35vh]"/>
+            <div className="mdmax-w-3xl  lg:max-w-4xl mx-auto">
+                <Toolbar initialData={document} />
+            </div>
+        </div>
+    );
+}
+
 export default DocumentIdPage;
